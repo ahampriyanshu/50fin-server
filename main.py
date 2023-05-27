c92daead42474aa58738915940def1995756d8e3
ahampriyanshu <ahampriyanshu@gmail.com>
2023-05-27 15:59:00 +0530

    feat: installed alembic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from routes import blog
@@ -32,14 +33,13 @@ async def shutdown():
     
     
 @app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc):
+async def http_exception_handler(_, exc):
     return JSONResponse(status_code=exc.status_code, content={"error": exc.detail})
 
 @app.exception_handler(Exception)
-async def http_exception_handler(request, exc):
+async def http_exception_handler(_, exc):
     print(exc)
     return JSONResponse(status_code=500, content={"error": "internal server error"})
-
 
 app.include_router(blog.router, tags=['Blog'], prefix='/api/posts')
 
@@ -47,13 +47,11 @@ app.include_router(blog.router, tags=['Blog'], prefix='/api/posts')
 def root():
     return {'status': 'OK'}
 
-@app.get('/')
-@app.post('/')
-@app.put('/')
-@app.put('/')
-def root():
-    return {
+# Catch-all route
+@app.route("/{full_path:path}")
+async def catch_all_handler(request: Request):
+    return JSONResponse(status_code=404, content={
         'Docmentation': 'https://five0fin.onrender.com/docs',
         'Preview': 'https://50fin.ahampriyanshu.com',
         'Code': 'https://github.com/ahampriyanshu/50fin-server',
-        }
+        })
